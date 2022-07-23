727f781132d22a4e64a6293c12a37ebaa15ac86d
Jonathan Ward <jward6t@gmail.com>
2022-07-22 22:19:24 -0500

    Updating files with server nearly wired up

--- server/src/main/java/com/noted/controller/NotedController.java
+++ server/src/main/java/com/noted/controller/NotedController.java
@@ -1,4 +1,22 @@
 package com.noted.controller;
 
+import com.noted.dao.NotedCardDao;
+import com.noted.model.NotedCard;
+import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class NotedController {
+
+    private final NotedCardDao notedCardDao;
+
+    public NotedController(NotedCardDao notedCardDao) {
+        this.notedCardDao = notedCardDao;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "http://localhost:8080/noted/entries", method = RequestMethod.POST)
+    public NotedCard createCard(@RequestBody NotedCard notedCard) { return notedCardDao.createNotedCard(notedCard); }
+
 }
