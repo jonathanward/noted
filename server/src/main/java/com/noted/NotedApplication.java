301dffd609ddc0e8e68b29a43c7d8d32863768c4
Jonathan Ward <jward6t@gmail.com>
2022-07-22 21:57:36 -0500

    Adding SpringBootApplication to application class

--- server/src/main/java/com/noted/NotedApplication.java
+++ server/src/main/java/com/noted/NotedApplication.java
@@ -1,4 +1,11 @@
 package com.noted;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class NotedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotedApplication.class, args);
+    }
 }
